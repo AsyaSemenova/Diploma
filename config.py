1e97c945d96e74442ae738e7f2a35266b183ee22
Anastasia <122823160+AsyaSemenova@users.noreply.github.com>
2023-05-09 18:30:56 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,3 @@
 bot_token = 'vk1.a.7WY0Y8e0m4bDKs6NVUsB0LashtRK9SN6DzAHw5Z8G9gT6zfHnnsbb5SRgL2NJ4SCnO_oU7M8fIQoV-eWJjwFKf5oaY8STzt-lSD9DQZkbii-I0yBYJATMk2wI5PcBzIkwEhkm64OovgThFxHNmQjUXl3x4o_nzJB5BOVnMYX-YDiYKjYP0Cl3vS18AqWfzzxrpiXoEeRZS4qyX7_DwN7sg'
 access_token =' '
 API = '5.131'
-
-client_id= '51591393'
